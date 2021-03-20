2bcbe4690f4760178314fb8da254a675d093ac1f
fidel bello <59423@bears.smcc.edu>
2021-03-20 13:27:01 -0400

    added starter files for project, secured databse with env file

---
+++ config/connection.js
@@ -0,0 +1,15 @@
+const Sequelize = require('sequelize');
+require('dotenv').config();
+
+const sequelize = new Sequelize(
+    process.env.DB,
+    process.env.USER,
+    process.env.PASSWORD,
+    {
+        host: 'localhost',
+        dialect: 'mysql',
+        port: 3306,
+    }
+);
+
+module.exports = sequelize
