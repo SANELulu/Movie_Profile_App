722062559d98a74c77ccbf45b507cf6eae94cde8
fidel bello <59423@bears.smcc.edu>
2021-03-22 11:19:51 -0400

    fixed a connection error

--- index.js
+++ index.js
@@ -1,34 +1,37 @@
-const sequelize = require('./config/connection');
-const express = require('express');
-const SequelizeStore = require('connect-session-sequelize')(session.Store)
 const path = require('path');
-const sequelize = require('./config/connection');
-const session = require('express-session')
+const express = require('express');
+const session = require('express-session');
 const exphs = require('express-handlebars');
+const controllers = require('./controllers');
 const helpers = require('./utils/helpers');
-const controllers = require('./controllers')
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-
-
-
-const app = express()
+const app = express();
 const PORT = process.env.PORT || 3001;
 
+const hbs = exphs.create({ helpers});
+
 const sess = {
-    secret: 'super, secret, secret',
+    secret: 'secret',
     cookie: {},
     resave: false,
     saveUnintialized: true,
     store: new SequelizeStore({
         db: sequelize
     })
-}
+};
 
 app.use(session(sess));
 
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(controllers);
 
-sequelize.sync({ force: false}).then(()=> {
-    app.listen(PORT, () => console.log( `now listening on: ${PORT}`))
+sequelize.sync({ force: false}).then(() => {
+    app.listen(PORT, ()=> console.log(`now listening on: ${PORT}`))
 });
-
