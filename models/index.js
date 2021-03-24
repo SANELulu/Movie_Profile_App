0b607f1be063663df7c82405b93d66645c83e9a6
fidel bello <59423@bears.smcc.edu>
2021-03-23 22:33:09 -0400

    added seed json and table for favorite movies and ready to make crud

--- models/index.js
+++ models/index.js
@@ -1,3 +1,13 @@
 const User = require('./user');
+const favMovie = require('./favmovies');
 
-module.exports = {User};
+User.hasMany(favMovie, {
+    foreignKey: 'user_id',
+    onDelete: 'Cascade'
+});
+
+favMovie.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+module.exports = {User, favMovie};
--- seed/seed.js
+++ seed/seed.js
@@ -1,7 +1,8 @@
 const sequelize = require('../config/connection');
-const { User, Project } = require('../models');
+const { User, favMovie} = require('../models');
 
 const userData = require('./userData.json');
+const favMovieData = require('./favmovieData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -11,6 +12,12 @@ const seedDatabase = async () => {
     returning: true,
   });
 
+  for (const favmovie of favMovieData) {
+    await favMovie.create({
+      ... favmovie,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+    });
+}
   process.exit(0);
 };
 
