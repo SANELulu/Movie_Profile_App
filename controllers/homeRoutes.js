d8578833154bd48bef8ce93de3a2353283fbc69c
Lucas Diaz <lucasdiaz22b@gmail.com>
2021-03-27 10:20:07 -0400

    working on routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -10,14 +10,14 @@ router.get('/', (req, res) => {
 
 router.get( '/login', async (req, res)  => {
 
-    // If the user is already logged in, redirect the request to another route
-    if (req.session.logged_in) {
-      res.redirect('/profile');
-      return;
-    }
+    // // If the user is already logged in, redirect the request to another route
+    // if (req.session.logged_in) {
+    //   res.redirect('/profile');
+    //   return;
+    // }
   
     res.render('login');
   });
-
+router.get('')
 
 module.exports = router;
