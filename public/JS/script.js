ef08875082901b1ca55e501dccb8c7bc0f75d55b
Mike Gordillo <mikegordillotattoo@gmail.com>
2021-04-01 19:41:26 -0400

    star.js

--- public/JS/script.js
+++ public/JS/script.js
@@ -13,22 +13,19 @@ const response = await fetch('/api/users/addfav', {
 // addFavMovie();
 
 
-const container = document.querySelector('.rating');
-const items = container.querySelectorAll('.rating-item')
-container.onclick = e => {
-    const elClass = e.target.classList;
-    if (elClass.contains('active')) {
-        items.forEach(
-            item => item.classList.remove('active')
-        );
-        console.log(e.target.getAttribute("data-rate"));
-    }
-    else {
-        items.forEach(
-            item => item.classList.remove('active')
-        );
-        console.log(e.target.getAttribute("data-rate"));
-        elClass.add('active');
-    }
-};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
