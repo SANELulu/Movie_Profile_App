e129fada69640506ae02a2ea244853c7f9ae5208
fidel bello <59423@bears.smcc.edu>
2021-04-01 22:08:22 -0400

    added some stuff

--- controllers/homeroutes.js
+++ controllers/homeroutes.js
@@ -23,13 +23,8 @@ router.get('/profile', withAuth, async (req, res) => {
   try {
     // Find the logged in user based on the session ID
     const userData = await User.findByPk(req.session.user_id, {
-      attributes: { exclude: ['password'] },
-      include: [
-        {
-        model: favmovie,
-        attributes: ['favMovie'],
-      },
-    ]
+      attributes: { exclude: ['password'] }
+      
     });
 
     const user = userData.get({ plain: true });
--- models/user.js
+++ models/user.js
@@ -51,14 +51,6 @@ User.init({
             isAlphanumeric: true,
         }
     },
-    favoriteMovie: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        references: {
-            model: 'favMovie',
-            key: 'movie_name'
-        }
-},
 },
 {
     hooks: {
